33420e93959847ebbdede0d050d54b757ea37877
Ola Popoola <oladipupo.popoola@gmail.com>
2026-05-06 14:20:35 +0100

    added contructor classes and methods

--- MiniBanking/src/com/eccl/MiniBanking.java
+++ MiniBanking/src/com/eccl/MiniBanking.java
@@ -5,4 +5,32 @@ public class MiniBanking {
     private double depositAmount;
     private double withdrawAmount;
 
+
+    // constructor
+    public MiniBanking(){ }
+
+    // getters and setters: balance
+    public double getBalance(){
+        return balance;
+    }
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+
+    // getters and setters: depositAmount
+    public double getDepositAmount(){
+        return depositAmount;
+    }
+    public void setDepositAmount(double depositAmount){
+        this.depositAmount = depositAmount;
+    }
+
+    // getters and setters: withdrawAmount
+    public double getWithdrawAmount(){
+        return withdrawAmount;
+    }
+    public void setWithdrawAmount(double withdrawAmount){
+        this.withdrawAmount = withdrawAmount;
+    }
+
 }
--- MiniBanking/src/com/eccl/MiniBankingMain.java
+++ MiniBanking/src/com/eccl/MiniBankingMain.java
@@ -1,5 +1,7 @@
 package com.eccl;
 
+import java.util.Scanner;
+
 /** This is basically an implementation of a Banking (or ATM) system
  * 1. Use Bank Number and PIN for Auth
  * 2. Check Available Balance
@@ -10,7 +12,55 @@ package com.eccl;
  * */
 
 public class MiniBankingMain {
-    public static void main(String[] args) {
-        System.out.println("\nWelcome to London com.eccl.MiniBanking\n==============================\n");
+    public static void main(String[] args) throws IllegalStateException {
+        System.out.println("\nWelcome to London MiniBanking\n==============================\n");
+
+        // instantiate authentications variables
+        int customerNumber = 98765;
+        final int PIN = 1982;
+
+        // request customer credentials
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Your Customer Number: ");
+        int customerInput = scanner.nextInt();
+        System.out.print("Enter Customer PIN: ");
+        int customerPin = scanner.nextInt();
+
+        //validate credentials
+        if ((customerInput == customerNumber) && (customerPin == PIN)){
+            System.out.println("Customer Account confirmed!");
+            while(true){
+
+                System.out.println("What would you like to do today? ");
+                System.out.println("1. Check Account Balance.\n2. Make Deposit.\n3. Make Withdrawal. \n4. Print Transaction History. \n5. Log out of account. \n");
+                int choice = scanner.nextInt();
+
+                switch(choice){
+                    case 1:
+                        System.out.println("Your Account balance is: ");
+
+                    case 2:
+                        System.out.println("How much do you want to deposit no? ");
+
+                    case 3:
+                        System.out.println("How much would you like to withdraw now? ");
+
+                    case 4:
+                        System.out.println("Here is your transaction history. ");
+
+                    case 5:
+                        System.out.println("Account Logged Off.\n Thank you for using London MiniBanking");
+                        break;
+
+                    default:
+                        System.out.println("Unexpected value: " + choice + ". Please enter between 1 and 5.");
+                        break;
+                }
+
+            }
+        } else {
+            System.out.println("Customer has no account with us!");
+        }
+
     }
 }
