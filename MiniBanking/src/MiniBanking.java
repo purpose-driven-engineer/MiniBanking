32297d054e5586dc95f4171db162205d3d8f5429
Ola Popoola <oladipupo.popoola@gmail.com>
2026-05-06 12:31:26 +0100

    seeding the banking app

---
+++ MiniBanking/src/MiniBanking.java
@@ -0,0 +1,18 @@
+
+
+/** This is basically an implementation of a Banking (or ATM) system
+ * 1. Use Bank Number and PIN for Auth
+ * 2. Check Available Balance
+ * 3. Make Deposit
+ * 4. Make Withdrawals
+ * 5. Print Mini Transaction History
+ * 6. Log out of account
+ * */
+
+public class MiniBanking {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to MiniBanking");
+    }
+
+}
